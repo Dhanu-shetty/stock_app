6dc8bdbcfcbb3180021155e8abce900252289e06
Dhanu-shetty <dhananjay.shetty50@gmail.com>
2021-10-01 22:59:09 +0530

    added thredinf

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ Created on Sun Sep 26 00:53:18 2021
 
 @author: dhanu
 """
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 26 00:53:18 2021
+
+@author: dhanu
+"""
 from nsetools import Nse
 nse = Nse()
-
+import threading
 import sched, time
 s = sched.scheduler(time.time, time.sleep)
 
@@ -38,10 +45,24 @@ def check_condition(value,condition):
                 return 0
     
 
+def process_data():
+    begin = time.time()
+    for tupul in user:
+        orders_by_id = stock_db_api.get_order_by_ID(a,tupul[0])
+        for tupul1 in orders_by_id:
+            value = nse.get_quote(tupul1[2])["lastPrice"]
+            if (check_condition(value,tupul1[3]))== 1:
+                print("condition true notification to user")
+            else:
+                print("no notification needed")
+    end = time.time()
 
-for tupul in user:
-    #print(tupul[0])
-    orders_by_id = stock_db_api.get_order_by_ID(a,tupul[0])
+    print("Total time taken in : process_data ->", end - begin)
+
+
+
+
+def Prosess_order_based_on_user(orders_by_id):
     for tupul1 in orders_by_id:
         value = nse.get_quote(tupul1[2])["lastPrice"]
         if (check_condition(value,tupul1[3]))== 1:
@@ -51,15 +72,24 @@ for tupul in user:
 
 
 
+thread_list =[]
+def process_data_thread():
+    begin = time.time()
+    for tupul in user:
+        orders_by_id = stock_db_api.get_order_by_ID(a,tupul[0])
+        t1 = threading.Thread(target=Prosess_order_based_on_user, args=(orders_by_id,))
+        t1.start()
+        thread_list.append(t1)
+        
+    while True:  
+        for i in thread_list:
+            i.join()  
+        break
+    end = time.time()
+    print("Total time taken in : process_data_thread ->", end - begin)
 
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    data = "My data read from the Web"
+    process_data()
+    process_data_thread()
 
